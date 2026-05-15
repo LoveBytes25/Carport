a240b99137534e4ed7b0012d9e724d574cace449
Eligoon <derpepind117@live.dk>
2026-05-15 14:06:14 +0200

    created entity class RoofType

---
+++ src/main/java/app/entities/RoofType.java
@@ -0,0 +1,23 @@
+package app.entities;
+
+public class RoofType {
+    int id;
+    String name;
+
+    public RoofType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
