c8a9f56bd29c962d4e50d6f59ccf6a742f275d1a
Eligoon <derpepind117@live.dk>
2026-05-19 20:06:05 +0200

    testing connectionpool in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,21 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import app.persistence.ConnectionPool;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.sql.Connection;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        ConnectionPool pool = ConnectionPool.getInstance();
+
+        try (Connection conn = pool.getConnection())
+        {
+            System.out.println("URL = " + System.getenv("JDBC_URL"));
+            System.out.println("USER=[" + System.getenv("JDBC_USER") + "]");
+            System.out.println("PASSWORD = " + System.getenv("JDBC_PASSWORD"));
+            System.out.println("Connected!");
         }
     }
 }
