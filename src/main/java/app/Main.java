4d2f981b39b7729fed804b3d545ffaae4927d8ee
Eligoon <derpepind117@live.dk>
2026-05-19 20:13:06 +0200

    main cleaned of test

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,21 +1,10 @@
 package app;
 
-import app.persistence.ConnectionPool;
-
-import java.sql.Connection;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        ConnectionPool pool = ConnectionPool.getInstance();
 
-        try (Connection conn = pool.getConnection())
-        {
-            System.out.println("URL = " + System.getenv("JDBC_URL"));
-            System.out.println("USER=[" + System.getenv("JDBC_USER") + "]");
-            System.out.println("PASSWORD = " + System.getenv("JDBC_PASSWORD"));
-            System.out.println("Connected!");
-        }
     }
 }
