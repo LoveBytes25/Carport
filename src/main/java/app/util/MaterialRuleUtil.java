892969f5b1037be417c41435fa39fcce8b238c93
Eligoon <derpepind117@live.dk>
2026-05-20 15:40:33 +0200

    updated materialcalculation rule,s now only including mm so there is no confusion

--- src/main/java/app/util/MaterialRuleUtil.java
+++ src/main/java/app/util/MaterialRuleUtil.java
@@ -5,72 +5,55 @@ public class MaterialRuleUtil {
     private MaterialRuleUtil() {
     }
 
-    public static int calculatePostQuantity(
-            double carportLength) {
-
-        return ((int)
-                Math.ceil(carportLength / 300.0)
-                + 1) * 2;
+    public static int calculatePostQuantity(double carportLength) {
+        return ((int) Math.ceil(carportLength / 3000.0) + 1) * 2;
     }
 
     public static double calculatePostLength() {
-
-        return 300;
+        return 3000;
     }
 
     public static int calculateBeamQuantity() {
-
         return 2;
     }
 
-    public static double calculateBeamLength(
-            double carportLength) {
-
+    public static double calculateBeamLength(double carportLength) {
         return carportLength;
     }
 
-    public static int calculateRafterQuantity(
-            double carportLength) {
-
-        return (int)
-                Math.ceil(carportLength / 55.0);
+    public static int calculateRafterQuantity(double carportLength) {
+        return (int) Math.ceil(carportLength / 550.0);
     }
 
-    public static double calculateRafterLength(
-            double carportWidth) {
-
+    public static double calculateFlatRafterLength(double carportWidth) {
         return carportWidth;
     }
 
-    public static int calculateRoofSheetQuantity(
-            double carportWidth) {
+    public static double calculatePitchedRafterLength(double carportWidth, double roofAngleDegrees) {
 
-        return (int)
-                Math.ceil(carportWidth / 100.0);
+        double radians =
+                Math.toRadians(
+                        roofAngleDegrees
+                );
+
+        return carportWidth /
+                Math.cos(radians);
     }
 
-    public static double calculateRoofSheetLength(
-            double carportLength) {
+    public static int calculateRoofSheetQuantity(double carportWidth) {
+        return (int) Math.ceil(carportWidth / 1000.0);
+    }
 
+    public static double calculateRoofSheetLength(double carportLength) {
         return carportLength;
     }
 
-    public static int calculateUnderSternSideQuantity() {
+    public static int calculateSideBoardQuantity() {
 
         return 2;
     }
 
-    public static int calculateUnderSternFrontQuantity() {
-
-        return 2;
-    }
-
-    public static int calculateOverSternSideQuantity() {
-
-        return 2;
-    }
-
-    public static int calculateOverSternFrontQuantity() {
+    public static int calculateFrontBoardQuantity() {
 
         return 2;
     }
