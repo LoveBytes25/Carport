a415c8578ef8637b4cd749f3c58ff1e2c43a5890
Eligoon <derpepind117@live.dk>
2026-05-21 19:01:39 +0200

    updated validation methods

--- src/main/java/app/util/Validator.java
+++ src/main/java/app/util/Validator.java
@@ -2,16 +2,25 @@ package app.util;
 
 public class Validator {
 
-    public static void validatePassword(String password, int min, int max) {
+    private static final int MIN_PASSWORD = 10;
+    private static final int MAX_PASSWORD = 20;
+
+    public static void validateEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+    }
+
+    public static void validatePassword(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password cannot be null");
         }
 
         int len = password.length();
 
-        if (len < min || len > max) {
+        if (len < MIN_PASSWORD || len > MAX_PASSWORD) {
             throw new IllegalArgumentException(
-                    "Password must be between " + min + " and " + max + " characters"
+                    "Password must be between 10 and 20 characters"
             );
         }
     }
