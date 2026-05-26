22498b626747be721e4e16765ff254be993d29a3
LoveBytes25 <keramsby@gmail.com>
2026-05-26 10:28:51 +0200

    Reverted ThymeleafConfig changes to old structure

--- src/main/java/app/config/ThymeleafConfig.java
+++ src/main/java/app/config/ThymeleafConfig.java
@@ -1,14 +1,8 @@
 package app.config;
 
-import io.javalin.http.Context;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class ThymeleafConfig {
 
@@ -26,23 +20,12 @@ public class ThymeleafConfig {
 
         resolver.setPrefix("templates/");
         resolver.setSuffix(".html");
-
         resolver.setTemplateMode(TemplateMode.HTML);
-
         resolver.setCharacterEncoding("UTF-8");
-
         resolver.setCacheable(false);
 
         TemplateEngine engine = new TemplateEngine();
         engine.setTemplateResolver(resolver);
         return engine;
-    }
-
-    public static WebContext buildWebContext(Context ctx) {
-        return new WebContext(
-                ctx.req(),   // HttpServletRequest
-                ctx.res(),   // HttpServletResponse
-                ctx.req().getServletContext()
-        );
     }
 }
