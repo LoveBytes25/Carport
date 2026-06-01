a4940414b4a6facb8d6ef56870f795acc0efa136
Eligoon <derpepind117@live.dk>
2026-06-01 10:30:11 +0200

    PriceController

---
+++ src/main/java/app/controllers/PriceController.java
@@ -0,0 +1,72 @@
+package app.controllers;
+
+import app.entities.CarportComponent;
+import app.entities.PriceSummary;
+
+import app.exceptions.DatabaseException;
+
+import app.persistence.ConnectionPool;
+
+import app.services.MaterialCalculationService;
+import app.services.PriceCalculationService;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import org.thymeleaf.TemplateEngine;
+
+import java.util.List;
+
+public class PriceController {
+
+    private final TemplateEngine templateEngine;
+
+    private final MaterialCalculationService materialService;
+
+    private final PriceCalculationService priceService;
+
+    public PriceController(TemplateEngine templateEngine, ConnectionPool connectionPool) {
+
+        this.templateEngine = templateEngine;
+
+        this.materialService = new MaterialCalculationService(connectionPool);
+
+        this.priceService = new PriceCalculationService(connectionPool);
+    }
+
+    public void register(Javalin app) {
+
+        app.post("/calculate-price", this::calculatePrice);
+    }
+
+    private void calculatePrice(Context ctx) throws DatabaseException {
+
+        double length = Double.parseDouble(ctx.formParam("length"));
+
+        double width = Double.parseDouble(ctx.formParam("width"));
+
+        String roofType = ctx.formParam("roofType");
+
+        double roofAngle = Double.parseDouble(ctx.formParam("roofAngle"));
+
+        List<CarportComponent> bom =
+                materialService.calculate(
+                        length,
+                        width,
+                        roofType,
+                        roofAngle
+                );
+
+        PriceSummary summary = priceService.calculatePrice(bom);
+
+        org.thymeleaf.context.Context thymeleafCtx = new org.thymeleaf.context.Context();
+
+        thymeleafCtx.setVariable("bom", bom);
+
+        thymeleafCtx.setVariable("priceSummary", summary);
+
+        String html = templateEngine.process("offer", thymeleafCtx);
+
+        ctx.html(html);
+    }
+}
